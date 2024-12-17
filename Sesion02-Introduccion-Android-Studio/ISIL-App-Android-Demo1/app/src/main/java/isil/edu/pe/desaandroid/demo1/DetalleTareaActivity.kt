1a227842153af2cc8fe361edafee682efb4a1c47
JCTOVARU <jctovarueda@gmail.com>
2024-12-16 22:37:11 -0500

    Sesion 5

---
+++ Sesion02-Introduccion-Android-Studio/ISIL-App-Android-Demo1/app/src/main/java/isil/edu/pe/desaandroid/demo1/DetalleTareaActivity.kt
@@ -0,0 +1,20 @@
+package isil.edu.pe.desaandroid.demo1
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class DetalleTareaActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        //Lineas comunes (por defecto)
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detalle_tarea)
+        //Recibir el nombre de la tarea desde la otra pantalla
+        val detalleTarea = intent.getStringExtra("DETALLE_TAREA")
+        //Mostrar en el TextView
+        val tvDetalleTareaActivity = findViewById<TextView>(R.id.tvDetalleTarea1)
+        tvDetalleTareaActivity.text = detalleTarea
+    }
+
+}
